2c3c94d650ea1b551f4e88ba0e828c22b6d340a2
FGtarts <hm.nashawi@gmail.com>
2026-05-04 15:06:07 +0300

    made blank screen

--- Project files/Snake.py
+++ Project files/Snake.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,sys
 pygame.init()
 
 screen  = pygame.display.set_mode((750, 750))
@@ -7,3 +7,11 @@ pygame.display.set_caption("Retro Snake Game")
 
 clock = pygame.time.Clock()
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
+    
