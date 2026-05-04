b6051e663a3c028694964366045485accfaedd70
FGtarts <hm.nashawi@gmail.com>
2026-05-04 23:38:42 +0300

    made snake grow and check for collisions

--- Project-files/Snake.py
+++ Project-files/Snake.py
@@ -31,11 +31,11 @@ class Food:
         while positon in snake_body:
             positon = self.generate_random_cell()
         return positon
-
 class Snake:
     def __init__(self):
         self.body = [Vector2(6,9),Vector2(5,9),Vector2(4,9)]
         self.direction = Vector2(1,0)
+        self.add_segment = False
 
     def draw(self):
         for segment in self.body:
@@ -43,26 +43,53 @@ class Snake:
             pygame.draw.rect(screen,DARK_GREEN,segment_rect,0,5)
     
     def update(self):
-        self.body = self.body[:-1]
-        self.body.insert(0, self.body[0]+self.direction)
+        self.body.insert(0, self.body[0] + self.direction)
+        if self.add_segment == True:
+            self.add_segment = False
+        else:
+            self.body = self.body[:-1]
 
+    def reset(self):
+        self.body = [Vector2(6,9),Vector2(5,9),Vector2(4,9)]
+        self.direction = Vector2(1,0)
 class Game:
     def __init__(self):
         self.snake = Snake()
         self.food = Food(self.snake.body)
+        self.state = "RUNNING"
 
     def draw(self):
         self.snake.draw()
         self.food.draw()
 
     def update(self):
-        self.snake.update()
-        self.check_collision_with_food()
+        if self.state == "RUNNING":
+            self.snake.update()
+            self.check_collision_with_food()
+            self.check_collision_with_border()
+            self.check_collision_with_tail()
 
     def check_collision_with_food(self):
         if self.snake.body[0] == self.food.positon:
             print("NOM NOM NOM")
             self.food.positon = self.food.generate_random_pos(self.snake.body)
+            self.snake.add_segment = True
+
+    def check_collision_with_border(self):
+        if self.snake.body[0].x == NUMBER_OF_CELLS or self.snake.body[0].x == -1:
+            self.game_over()
+        if self.snake.body[0].y == NUMBER_OF_CELLS or self.snake.body[0].y == -1:
+            self.game_over()
+
+    def check_collision_with_tail(self):
+        headless_body = self.snake.body[1:]
+        if self.snake.body[0] in headless_body:
+            self.game_over()
+
+    def game_over(self):
+        self.snake.reset()
+        self.food.positon = self.food.generate_random_pos(self.snake.body)
+        self.state = "PAUSED"
 
 #SCREEN STUFF
 screen  = pygame.display.set_mode((CELL_SIZE * NUMBER_OF_CELLS, CELL_SIZE * NUMBER_OF_CELLS))
@@ -84,6 +111,8 @@ while True:
             game.update()
 
         if event.type == pygame.KEYDOWN:
+            if game.state == "PAUSED":
+                game.state = "RUNNING"
             if event.key == pygame.K_UP and game.snake.direction != Vector2(0,1):
                 game.snake.direction = Vector2(0,-1)
             if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0,-1):
